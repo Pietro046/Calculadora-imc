60d4117c15ed8828476f5503d71f45c15b7c9b5e
Pietro Jesus <pietrojesus046@gmail.com>
2026-06-14 18:59:03 -0300

    Calculadora imc

---
+++ projeto/src/conversor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class conversor {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a temperatura em Celsius: ");
+        double celsius = scanner.nextDouble();
+        scanner.close();
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius+" graus Celsius é igual a "+fahrenheit+" graus Fahrenheit");
+    }
+}
+
+
+
---
+++ projeto/src/teste.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu peso em kg: ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Digite sua altura em metros: ");
+        double altura = scanner.nextDouble();
+
+        double imc = peso / (altura * altura);
+
+        System.out.printf("Seu imc é: %.2f%n", imc);
+
+        scanner.close();
+
+        if (imc < 18.5) {
+            System.out.println("Você está abaixo do peso.");
+        }
+        else if (imc <= 24.9) {
+            System.out.println("Você está com o peso adequado.");
+        }
+        else if (imc <= 29.9) {
+            System.out.println("Você está acima do Peso.");
+        }
+        else {
+            System.out.println("Você está com obesidade.");
+        }
+    }
+}
