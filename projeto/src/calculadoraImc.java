56324bb208a2532da954682828e818334c9f27d1
Pietro Jesus Scarpelim <pietrojesus046@gmail.com>
2026-06-15 09:52:02 -0300

    Rename class 'teste' to 'calculadoraImc'

--- projeto/src/calculadoraImc.java
+++ projeto/src/calculadoraImc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class teste {
+public class calculadoraImc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite seu peso em kg: ");
